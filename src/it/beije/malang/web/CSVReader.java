5e09cb47d887a3e78560af65572538d86fd3cf25
et994 <55279734+et994@users.noreply.github.com>
2019-10-16 16:37:06 +0200

    test

---
+++ src/it/beije/malang/web/CSVReader.java
@@ -0,0 +1,32 @@
+package it.beije.malang.web;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVReader {
+
+    public static void main(String[] args) {
+
+        String csvFile = "C:/temp/country.csv";
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] country = line.split(cvsSplitBy);
+
+                System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
