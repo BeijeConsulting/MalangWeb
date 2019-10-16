65a6b95300dfffe8e75e75285ac893b334ba413b
Ferulligabriele <55279860+Ferulligabriele@users.noreply.github.com>
2019-10-16 16:44:09 +0200

    2019-10-16

---
+++ src/it/beije/malang/web/Contatto.java
@@ -0,0 +1,63 @@
+package it.beije.malang.web;
+
+public class Contatto {
+	
+	private int id;
+	private String cognome;
+	private String nome;
+	private String telefono;
+	private String email;
+	private String età;
+	
+	public String getEtà() {
+		return età;
+	}
+	public void setEtà(String età) {
+		this.età = età;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("cognome : ").append(cognome).append('\n');
+		builder.append("nome : ").append(nome).append('\n');
+		builder.append("telefono : ").append(telefono).append('\n');
+		builder.append("email : ").append(email).append('\n');
+		builder.append("età : ").append(età).append('\n');
+		return builder.toString();
+	}
+}
--- src/it/beije/malang/web/TestServlet.java
+++ src/it/beije/malang/web/TestServlet.java
@@ -1,12 +1,20 @@
 package it.beije.malang.web;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import it.beije.malang.web.Contatto;
+import it.beije.malang.web.importfile;
 /**
  * Servlet implementation class TestServlet
  */
@@ -26,8 +34,16 @@ public class TestServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		ArrayList<Contatto> lista = importfile.salvaInArray("C:\\temp\\rubrica.txt");
+		
+		for(Contatto c : lista) {
+			response.getWriter().append(c.getNome() + " " + c.getCognome() + " " + c.getTelefono() + " " + c.getEmail() + "\n");
+			
+		}
+	
 	}
 
 	/**
