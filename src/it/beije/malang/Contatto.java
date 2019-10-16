d38efc2fbc51ed03a067a646f179ed09f67200ec
Fulginiti <Fulginiti@LAPTOP-EE4JK8P8>
2019-10-16 16:41:40 +0200

    first commit web

---
+++ src/it/beije/malang/Contatto.java
@@ -0,0 +1,66 @@
+package it.beije.malang;
+
+public class Contatto {
+		
+		private int id;
+		private String cognome;
+		private String nome;
+		private String telefono;
+		private String email;
+		private String eta;
+		
+		public String getEta() {
+			return eta;
+		}
+		public void setEta(String eta) {
+			this.eta = eta;
+		}
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		
+		public String getCognome() {
+			return cognome;
+		}
+		public void setCognome(String cognome) {
+			this.cognome = cognome;
+		}
+		
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		
+		public String getTelefono() {
+			return telefono;
+		}
+		public void setTelefono(String telefono) {
+			this.telefono = telefono;
+		}
+		
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		
+		
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			builder.append("cognome : ").append(cognome).append('\n');
+			builder.append("nome : ").append(nome).append('\n');
+			builder.append("telefono : ").append(telefono).append('\n');
+			builder.append("email : ").append(email).append('\n');
+			builder.append("eta  : ").append(eta).append('\n');
+			return builder.toString();
+		}
+	}
+
+
+
--- src/it/beije/malang/web/TestServlet.java
+++ src/it/beije/malang/web/TestServlet.java
@@ -1,12 +1,19 @@
 package it.beije.malang.web;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import it.beije.malang.Contatto;
 /**
  * Servlet implementation class TestServlet
  */
@@ -21,14 +28,64 @@ public class TestServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+    
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("contatti: \n");
+		ArrayList<Contatto> lista = salvaInArray("C:\\Users\\Fulginiti\\Desktop\\ciao.txt");
+		//response.getWriter().append(salvaInArray("C:\\Users\\Fulginiti\\Desktop\\ciao.txt"));
+		for(Contatto c : lista) {
+			response.getWriter().append(c.getNome() + " " + c.getCognome() + " " + c.getTelefono() + " " + c.getEmail() + "\n");
+		}
+		
+		
 	}
+public static ArrayList<Contatto> salvaInArray(String pathFile) {
+		
+		File f = new File(pathFile);
+		System.out.println(f.exists());
+		
+		 ArrayList<Contatto> utente = new ArrayList<Contatto>();
+		
+			try {
+		
+				FileReader fileReader = new FileReader(f);
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
+				
+				Contatto contatto = null;
+				
+				StringTokenizer tokenizer = null;
+				
+				while (bufferedReader.ready()) {
+					tokenizer = new StringTokenizer(bufferedReader.readLine(), ";");
+					contatto = new Contatto();
+					contatto.setNome(tokenizer.nextToken());
+					contatto.setCognome(tokenizer.nextToken());
+					contatto.setTelefono(tokenizer.nextToken());
+					contatto.setEmail(tokenizer.nextToken());
+	
+					utente.add(contatto);
+					
+				}
+				
+			bufferedReader.close();	
+			fileReader.close();	
+			
+			
+			
+			}catch (FileNotFoundException FnFdExc){
+				System.out.println("file non trovato");
+			}catch (IOException IOExc) {
+				System.out.println("IOException");
+			}
+			
+			return utente;		
+}	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
